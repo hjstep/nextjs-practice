edf305e258f1a2e25c9b81fea5d547a5f4ad9411
Heejeong Hwang <heejeongdev@gmail.com>
2024-01-20 17:45:40 +0900

    Update: 깃허브액션 테스트하기위한 커밋

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { GetServerSideProps } from 'next'
 
 export default function Index({text}: { text: string}) {
-  return (
-    <div>
-        NextJs {text}
-    </div>
-  )
+  return <div>{text} NextJs world!</div>
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
